6d7df50d42f636b43f98888240fd85143376c22c
A5harib <asharibhashmi7@gmail.com>
2025-04-15 02:54:42 +0500

    day1

---
+++ routes/users.js
@@ -0,0 +1,49 @@
+const express = require(`express`);
+const router = express.Router();
+const path = require(`path`);
+
+router.get(`/new`, (req, res) => {
+  res.sendFile(path.join(__dirname, `../views/form.html`));
+});
+
+router.post("/", (req, res) => {
+  const { name, email, password } = req.body;
+  res.send(` <h1>User Created:</h1>
+    <p><strong>Name:</strong> ${name}</p>
+    <p><strong>Email:</strong> ${email}</p>
+    <p><strong>Password:</strong> ${password}</p>
+  `);
+});
+
+router.get(`/:id`, (req, res) => {
+  const id = req.params.id;
+  res.send(`Read specific id data: ${id}`);
+});
+router.get(`/:name/:email/:password`, (req, res) => {
+  const name = req.params.name;
+  const email = req.params.email;
+  const password = req.params.password;
+  let valid = "youre not asharib!!!!!!!";
+  if (password === "localhost") {
+    valid = "oh hey you are asharib!";
+  }
+  res.send(` <h1>User Created:</h1>
+    <p><strong>Name:</strong> ${name}</p>
+    <p><strong>Email:</strong> ${email}</p>
+    <p><strong>Password:</strong> ${password}</p>
+    
+    <h1><p><strong>Validation:</strong> ${valid}</p> </h1>
+  `);
+});
+
+const users = [
+  { id: 1, name: `John` },
+  { id: 2, name: `Jane` },
+  { id: 3, name: `Doe` },
+];
+router.param(`id`, (req, res, next, id) => {
+  req.user = users[id];
+
+  next();
+});
+module.exports = router;
--- server.js
+++ server.js
@@ -1,4 +1,20 @@
 const express = require("express");
 const app = express();
+const path = require(`path`);
+app.set(`view engine`, `ejs`);
+app.use(login);
+app.use(express.urlencoded({extended:true}))
 
+app.get("/", (req, res) => {
+  //   res.send(`<h1 class="">hi</h1>`);
+  res.sendFile(path.join(__dirname, `./views/index.html`));
+});
+
+const userRouter = require(`./routes/users`);
+app.use("/users", userRouter);
+
+function login(req, res, next) {
+  console.log(req.originalUrl);
+  next();
+}
 app.listen(5173);
